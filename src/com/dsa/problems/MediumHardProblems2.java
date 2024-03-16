3e4ddada8c8b5dbb7765e791a6c8b01e9b655db7
tejas-2552 <Rtejas3730@gmail.com>
2024-03-17 02:03:20 +0530

    Medium Hard Problems Update

--- src/com/dsa/problems/MediumHardProblems2.java
+++ src/com/dsa/problems/MediumHardProblems2.java
@@ -1,8 +1,10 @@
 package com.dsa.problems;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class MediumHardProblems2 {
@@ -11,6 +13,12 @@ public class MediumHardProblems2 {
 
 		// https://www.youtube.com/watch?v=TidC-dG9C3s&list=PLFdAYMIVJQHO1paovM-tu2vtGzQU72z_U&index=1&t=5s
 		System.out.println(validString("AABBCDDDD"));
+
+		// https://www.youtube.com/watch?v=lGDyeD597RA&list=PLFdAYMIVJQHO1paovM-tu2vtGzQU72z_U&index=2&t=42s
+		System.out.println(findPairsCount(new int[] { 1, 5, 3, 4, 2 }, 2));
+
+		// https://www.youtube.com/watch?v=uX0-xyPkR2w&list=PLFdAYMIVJQHO1paovM-tu2vtGzQU72z_U&index=3
+		System.out.println(longestPalindrome("EBBABAD"));
 	}
 
 	public static boolean validString(String str) {
@@ -31,11 +39,70 @@ public class MediumHardProblems2 {
 		if (cnt.size() == 2) {
 			for (Entry<Integer, Integer> e : cnt.entrySet()) {
 				System.out.println(e.getKey() + " : " + e.getValue());
-				if (e.getKey() == 1 && e.getValue() == 1 || cnt.containsKey(e.getKey() - 1)  && e.getValue() == 1) {
+				if (e.getKey() == 1 && e.getValue() == 1 || cnt.containsKey(e.getKey() - 1) && e.getValue() == 1) {
 					return true;
-				} 	
+				}
 			}
 		}
 		return false;
+	}
+
+	public static int findPairsCount(int[] arr, int target) {
+
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < arr.length; i++) {
+			set.add(arr[i]);
+		}
+
+		int pairCount = 0;
+		for (Integer i : set) {
+			System.out.println("i : " + i + "target : " + target + " = " + (i - target));
+			if (set.contains(i - target)) {
+				pairCount++;
+			}
+		}
+		return pairCount;
+	}
+
+	public static String longestPalindrome(String str) {
+
+		String lps = "";
+		if (str.length() <= 1) {
+			return str;
+		}
+		for (int i = 1; i < str.length(); i++) {
+			int high = i;
+			int low = i;
+			while (str.charAt(high) == str.charAt(low)) {
+				high++;
+				low--;
+				if (low == -1 || high == str.length()) {
+					break;
+				}
+			}
+
+			String lpsStr = str.substring(low + 1, high);
+			if (lpsStr.length() > lps.length()) {
+				lps = lpsStr;
+			}
+
+			high = i;
+			low = i - 1;
+			while (str.charAt(high) == str.charAt(low)) {
+				high++;
+				low--;
+				if (low == -1 || high == str.length()) {
+					break;
+				}
+			}
+
+			lpsStr = str.substring(low + 1, high);
+			if (lpsStr.length() > lps.length()) {
+				lps = lpsStr;
+			}
+
+		}
+
+		return lps;
 	}
 }
