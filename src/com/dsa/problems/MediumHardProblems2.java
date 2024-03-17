339b86fe8c5fc530a5245f870eb313419648ea21
tejas-2552 <Rtejas3730@gmail.com>
2024-03-17 20:20:35 +0530

    Medium Hard Problems Update

--- src/com/dsa/problems/MediumHardProblems2.java
+++ src/com/dsa/problems/MediumHardProblems2.java
@@ -1,7 +1,10 @@
 package com.dsa.problems;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -19,6 +22,14 @@ public class MediumHardProblems2 {
 
 		// https://www.youtube.com/watch?v=uX0-xyPkR2w&list=PLFdAYMIVJQHO1paovM-tu2vtGzQU72z_U&index=3
 		System.out.println(longestPalindrome("EBBABAD"));
+
+		// https://www.youtube.com/shorts/Hzmc2TtYC18
+		System.out.println(lastStoneWeight(new int[] { 7, 6, 7, 6, 9 }));
+
+		// https://www.youtube.com/watch?v=iXLMMbdjeNM&list=PLFdAYMIVJQHO1paovM-tu2vtGzQU72z_U&index=5
+		System.out.println(elementInRotatedSubArr(new int[] { 6, 7, 8, 1, 2, 3, 4, 5 }, 8, 0, 7));
+		
+		printGroupAnagrams();
 	}
 
 	public static boolean validString(String str) {
@@ -105,4 +116,89 @@ public class MediumHardProblems2 {
 
 		return lps;
 	}
+
+	public static int lastStoneWeight(int[] arr) {
+
+		Arrays.sort(arr);
+		int size;
+		if (arr.length % 2 == 0) {
+			size = arr.length / 2;
+		} else {
+			size = arr.length / 2 + 1;
+		}
+		int[] countArr = new int[size];
+		int cnt = 0;
+		int lsw = 0;
+		for (int i = arr.length - 1; i >= 0; i = i - 2) {
+			int l = 0;
+			if (i == 0) {
+				l = arr[i] - 0;
+			} else {
+				l = arr[i] - arr[i - 1];
+			}
+			countArr[cnt++] = l;
+		}
+		System.out.println(Arrays.toString(countArr));
+		/*
+		 * for (int j = 0; j < size; j++) { int l = countArr[j]; if (l < lsw) { lsw =
+		 * lsw - l; } else { lsw = l - lsw; } }
+		 */
+		if (countArr.length > 1) {
+			lastStoneWeight(countArr);
+		}
+		return countArr[0];
+	}
+
+	public static int elementInRotatedSubArr(int[] arr, int target, int left, int right) {
+
+		if (left > right) {
+			return -1;
+		}
+
+		int mid = left + ((right - left) / 2);
+
+		if (arr[mid] == target) {
+			return mid;
+		}
+
+		if (arr[mid] >= arr[left]) {
+
+			if (arr[left] <= target && target <= arr[mid]) {
+				return elementInRotatedSubArr(arr, target, left, mid - 1);
+			} else {
+				return elementInRotatedSubArr(arr, target, mid + 1, right);
+			}
+		} else {
+			if (arr[right] >= target && target >= arr[mid]) {
+				return elementInRotatedSubArr(arr, target, mid + 1, right);
+			} else {
+				return elementInRotatedSubArr(arr, target, left, mid - 1);
+			}
+		}
+
+	}
+
+	public static void printGroupAnagrams() {
+
+		String[] str = { "cat", "tac", "bat", "nat", "ate", "tea", "tan" };
+		Map<String, List<String>> map = new HashMap<>();
+		for (String s : str) {
+			char[] ch = s.toCharArray();
+			Arrays.sort(ch);
+			String sorted = new String(ch);
+			List<String> list;
+			if (!map.containsKey(sorted)) {
+				list = new LinkedList<>();
+				list.add(s);
+				map.putIfAbsent(sorted, list);
+			} else {
+				list = map.get(sorted);
+				list.add(s);
+				map.put(sorted, list);
+			}
+		}
+
+		map.forEach((k, v) -> System.out.println(k + " : " + v));
+	}
+
 }
