cb238f98303b37afd44d7d665610abe58d6632c7
tejas-2552 <Rtejas3730@gmail.com>
2024-03-20 20:15:19 +0530

    Binary Search Tree Update

--- src/com/dsa/ds/BinarySearchTree.java
+++ src/com/dsa/ds/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package com.dsa.ds;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /*
  * What is binary search tree?
  * - It is a type of binary tree where the data is organized in ordered manner which helps in faster insertion and search.
@@ -11,7 +15,7 @@ public class BinarySearchTree {
 
 	private TreeNode root;
 
-	public static class TreeNode {
+	public class TreeNode {
 
 		private int data;
 		private TreeNode left;
@@ -22,12 +26,51 @@ public class BinarySearchTree {
 		}
 
 	}
-	
-	public TreeNode insert(TreeNode node, int val) {
-		
+
+	public void insert(int val) {
+		root = insert(root, val);
 	}
-	
+
+	public TreeNode insert(TreeNode node, int val) {
+
+		if (node == null) {
+			node = new TreeNode(val);
+			return node;
+		}
+
+		if (val < node.data) {
+			node.left = insert(node.left, val);
+		} else {
+			node.right = insert(node.right, val);
+		}
+
+		return node;
+	}
+
+	public void preOrderTraversal(TreeNode root) {
+
+		if (root == null) {
+			return;
+		}
+		Queue<TreeNode> q = new LinkedList<>();
+		q.offer(root);
+		while (!q.isEmpty()) {
+			TreeNode node = q.poll();
+			if (node != null) {
+				System.out.print(node.data + " ");
+				q.offer(node.left);
+				q.offer(node.right);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hll");
+		BinarySearchTree bst = new BinarySearchTree();
+		bst.insert(5);
+		bst.insert(3);
+		bst.insert(7);
+		bst.insert(1);
+
+		bst.preOrderTraversal(bst.root);
 	}
 }
