a121d904f8366802bfafead73343bf861fb8ee54
tejas-2552 <Rtejas3730@gmail.com>
2024-04-02 00:39:09 +0530

    Synchronization based on same Emplyee locks

--- src/com/dsa/multithreading/SynchronizationExample.java
+++ src/com/dsa/multithreading/SynchronizationExample.java
@@ -1,26 +1,67 @@
 package com.dsa.multithreading;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.dsa.model.Employee;
+
 public class SynchronizationExample {
+
+	List<Employee> empList = new ArrayList<>();
 
 	public static void main(String args[]) {
 
 		System.out.println("Main is starting.");
-
-		StackClass stack = new StackClass(5);
-
-		new Thread(() -> {
-			int counter = 0;
-			while (++counter < 10) {
-				System.out.println("Pushed : " + stack.push(100));
-			}
-		}, "Pusher").start();
-
-		new Thread(() -> {
-			int counter = 0;
-			while (++counter < 10) {
-				System.out.println("Poped : " + stack.pop());
-			}
-		}, "Popper").start();
-		System.out.println("Exiting Main.");
+		List<String> nameList = Arrays.asList("Abc", "Abc", "Abc", "Abc", "Abcd", "Abce", "Abcf", "Abc", "Abcd", "Abce",
+				"Abcf");
+		SynchronizationExample synClass = new SynchronizationExample();
+		synClass.initEmployes();
+		for (String name : nameList) {
+			new Thread(() -> {
+				synClass.performEmployeeOp(name);
+			}, "Thread").start();
+		}
+		System.out.println("Existing Main Method.");
 	}
+
+	// Synchronization according to user
+
+	public void initEmployes() {
+		Employee emp1 = new Employee("Abc", 1000, 10);
+		Employee emp2 = new Employee("Abcd", 1000, 10);
+		Employee emp3 = new Employee("Abce", 1000, 10);
+		Employee emp4 = new Employee("Abcf", 1000, 10);
+
+		empList.add(emp1);
+		empList.add(emp2);
+		empList.add(emp3);
+		empList.add(emp4);
+	}
+
+	public Employee findByName(String name) {
+		for (Employee emp : empList) {
+			if (emp.getName().equalsIgnoreCase(name)) {
+				return emp;
+			}
+		}
+		return null;
+	}
+
+	public synchronized void performEmployeeOp(String name) {
+		Employee emp = findByName(name);
+		synchronized (emp) {
+			System.out.println("In Syn block for emp : " + name);
+			try {
+				if(name.equalsIgnoreCase("Abc")) {
+					Thread.sleep(5000);					
+				} else {
+					//Thread.sleep(3000);					
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
--- src/com/dsa/problems/MediumHardProblems4.java
+++ src/com/dsa/problems/MediumHardProblems4.java
@@ -2,8 +2,11 @@ package com.dsa.problems;
 
 public class MediumHardProblems4 {
 
-	
 	public static void main(String args[]) {
+		
+		// All possible subset
+		// https://www.youtube.com/watch?v=3tpjp5h3M6Y&list=PLFdAYMIVJQHO1paovM-tu2vtGzQU72z_U&index=19
+		
 		
 	}
 	
